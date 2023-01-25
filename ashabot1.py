5e6056ece3a26b746108dd944b89490750cd2aea
Mutiul Muhaimin <mutiuldrive@gmail.com>
2023-01-25 16:15:21 +0600

    Fourth Commit

--- ashabot1.py
+++ ashabot1.py
@@ -12,7 +12,9 @@ from db_process import process_db
 
 #df1 = df.values.tolist()
 
-chatbot = ChatBot("asha1")
+#chatbot = ChatBot("asha1", storage_adapter='chatterbot.storage.SQLStorageAdapter', database_uri='sqlite:///database.sqlite3')
+#chatbot = ChatBot("Johnny Five", read_only=True)
+chatbot = ChatBot("asha1", logic_adapters=[{'import_path': 'chatterbot.logic.BestMatch', 'default_response': 'I am sorry, but I do not understand.', 'maximum_similarity_threshold': 0.65}])
 #chatbot.storage.drop()     # use to unlearn if needed
 
 
@@ -21,11 +23,13 @@ chatbot = ChatBot("asha1")
 #query = input("> ")
 def chat_resp(query):
     # chatbot.storage.drop()     # use to unlearn if needed
-    # CORPUS_FILE = "E:\\grameen\\chatbot\\my_code_1\\data_air\\db_mental.db"     # path to dataset
+    # CORPUS_FILE = "E:\\grameen\\chatbot\\my_code_1\\data_air\\db_mental.db"       # path to dataset
     # cleaned_corpus = process_db(CORPUS_FILE, 'Faq')
     # trainer = ListTrainer(chatbot)
     # trainer.train(cleaned_corpus)
-    return(chatbot.get_response(query))
+    resp = chatbot.get_response(query)
+    conf = resp.confidence
+    return(resp, conf)     #
 """
     cleaned_corpus = divideFile(CORPUS_FILE)
     trainer = ListTrainer(chatbot)
